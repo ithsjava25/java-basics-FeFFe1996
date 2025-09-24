2e3154278a1e7680ce943b5ba3ab08dcf6e3fa2e
feer0004 <felix.eriksson1996@hotmail.com>
2025-09-24 23:00:04 +0200

    testing code

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,86 +5,64 @@ import com.example.api.ElpriserAPI;
 import java.text.DecimalFormat;
 
 import java.time.LocalDate;
-
+import java.util.Arrays;
 
 
 public class Main {
     public static void main(String[] args) {
         ElpriserAPI elpriserAPI = new ElpriserAPI();
-        String date =  LocalDate.now().toString();
-        String zone =  "SE3";
+        String date = LocalDate.now().toString();
+        String zone = "SE3";
+
         int dataLength = elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).size();
+        Double[] splitArray = new Double[dataLength];
         String[] dataArray = new String[dataLength];
-        String[] splitArray = new String[dataLength];
-        String[] timeArray = new String[dataLength];
-        for(int i = 0; i < dataLength; i++){
-            dataArray[i] = String.valueOf(elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(i));
-
-            //splitArray[i] = dataArray[i].split("[,=:]")[1];
-
-
-       }
-//        for(int j = 0; j < dataLength; j++){
-//
-//            timeArray[j] = String.valueOf(splitArray[7]); //finds the start time of the array
-//        }
-
-        getMean(dataArray, splitArray, dataLength);
-        //getMaxMin(pricesArray, timeArray);
-    }
-
-
-    public static void getCurrentDay(String zone){
-        ElpriserAPI elpriserAPI = new ElpriserAPI();
-        LocalDate today = LocalDate.now();
-        LocalDate tomorrow = today.plusDays(1);
-        System.out.println(elpriserAPI.getPriser(today, ElpriserAPI.Prisklass.valueOf(zone)).size());
-    }
-
-    private static void getMean(String[] dataArray, String[] splitArray, int dataLength) { //get the mean value of the day prices
+        System.out.println(elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)));
+        DecimalFormat df = new DecimalFormat("##,00");
         for (int i = 0; i < dataLength; i++) {
-            splitArray[i] = dataArray[i].split("[:=,]")[1];
+            System.out.println(elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(i));
         }
-
-        DecimalFormat df = new DecimalFormat("#,00");
-
         double value = 0;
-        for (int a = 0; a < dataLength; a++){ //for loop to add the values of pricesArray
-            value += Double.parseDouble(splitArray[a]);
+        double minValue = 1;
+        double maxValue = 0;
+        String minTimeStart = "";
+        String minTimeEnd = "";
+        String maxTimeStart = "";
+        String maxTimeEnd = "";
+        for (int a = 0; a < dataLength; a++) { //for loop to add the values of pricesArray
+            value += elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).sekPerKWh();
+            if(minValue > elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).sekPerKWh()){
+                minValue = elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).sekPerKWh();
+                minTimeStart = String.valueOf(elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).timeStart());
+                minTimeEnd = String.valueOf(elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).timeEnd());
+                minTimeStart = minTimeStart.substring(minTimeStart.indexOf("T")+1, minTimeStart.indexOf("T")+3);
+                minTimeEnd =  minTimeEnd.substring(minTimeEnd.indexOf("T")+1, minTimeEnd.indexOf("T")+3);
+            } else if(maxValue < elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).sekPerKWh()){
+                maxValue = elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).sekPerKWh();
+                maxTimeStart = String.valueOf(elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).timeStart());
+                maxTimeEnd = String.valueOf(elpriserAPI.getPriser(date, ElpriserAPI.Prisklass.valueOf(zone)).get(a).timeEnd());
+                maxTimeStart = maxTimeStart.substring(maxTimeStart.indexOf("T")+1, maxTimeStart.indexOf("T")+3);
+                maxTimeEnd = maxTimeEnd.substring(maxTimeEnd.indexOf("T")+1, maxTimeEnd.indexOf("T")+3);
+            }
         }
-        double meanValue = (value / dataLength) * 100;
-        String mean = String.valueOf(df.format(meanValue));
+        value = value / dataLength;
+        double mean = value*100;
+        int meanPrice = (int) mean;
+        minValue *= 100;
+        maxValue *= 100;
         System.out.println(" ");
-        System.out.println("medelpris: " + mean + " öre");
+        System.out.println("medelpris " + meanPrice);
+        System.out.println("lägsta pris " + minValue + " " + minTimeStart + "-" + minTimeEnd);
+        System.out.println("högsta pris " + maxValue + " " + maxTimeStart + "-" + maxTimeEnd);
     }
 
-//    private static void getMaxMin(double[] pricesArray, String[] timeArray) { //get the max and min prices fpr the day.
-//        double maxValue = 0.0;
-//        double minValue = 0.0;
-//        String maxValueTime = "";
-//        String minValueTime = "";
-//
-//        for (int a = 0; a < 24; a++){
-//
-//            if (pricesArray[a] > maxValue){ //finds the max value and the time it is.
-//                maxValue = pricesArray[a];
-//                maxValueTime = timeArray[a];
-//
-//            }
-//            else if (pricesArray[a] < minValue){ //same as max but for the lowest value of the day.
-//                minValue = pricesArray[a];
-//                minValueTime = timeArray[a];
-//            }
-//        }
-//        printText(maxValueTime, minValueTime, maxValue, minValue);
+
+//    public static void getCurrentDay(String zone) {
+//        ElpriserAPI elpriserAPI = new ElpriserAPI();
+//        LocalDate today = LocalDate.now();
+//        LocalDate tomorrow = today.plusDays(1);
+//        System.out.println(elpriserAPI.getPriser(today, ElpriserAPI.Prisklass.valueOf(zone)));
 //    }
-
-    private static void printText(String maxValueTime, String minValueTime, double maxValue, double minValue) {
-        maxValueTime = maxValueTime.replace("T", " ");
-        minValueTime = minValueTime.replace("T", " ");
-        System.out.println("Max: " + maxValue + " at: "+ maxValueTime);
-        System.out.println("Min: " + minValue +  " at: "+ minValueTime);
-    }
 }
 
 
